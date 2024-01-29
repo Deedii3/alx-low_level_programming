1b49a9f982fdcbf22258aa2908ddd83ba190d9fd
Deedii3 <agafie4divine@gmail.com>
2024-01-29 10:10:22 -0800

    Running C

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - reverses alphabet
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 122; n > 96; n--)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+
+	return (0);
+}
