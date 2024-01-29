f87f3b6324667af58dd6194179bf5d4b497906e0
Deedii3 <agafie4divine@gmail.com>
2024-01-29 09:59:24 -0800

    Running C

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints 0 to 9
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 48; n < 58; n++)
+	{
+		putchar(n);
+	}
+	putchar('\n');
+}
