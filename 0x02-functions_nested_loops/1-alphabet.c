f53e65a9fb903abe7003f3fd4c95db12b3679057
Deedii3 <agafie4divine@gmail.com>
2024-02-01 15:44:58 -0800

    Running C

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase
+ *
+ * Return: void
+ */
+void print_alphabet(void)
+{
+	int n;
+
+	for (n = 97; n <= 122; n++)
+	{
+		_putchar(n);
+	}
+	_putchar('\n');
+}
