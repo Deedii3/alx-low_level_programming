562bf2f46944306ea9608e8f52e31eeb960a0fd8
Deedii3 <agafie4divine@gmail.com>
2024-02-24 13:53:16 -0800

    Running C

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_sign - prints the sign of a number
+ * @n - Integer
+ *
+ * Return: 1(positive), 0(Zero) and -1(negative)
+ */
+int print_sign(int n)
+{
+	if (n > 0)
+	{
+		putchar(43);
+		return(1);
+	}
+	else if (n == 0)
+	{
+		putchar(48);
+		return(0);
+	}
+	else
+	{
+		putchar(45);
+		return(-1);
+	}
+}
